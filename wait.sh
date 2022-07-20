671420742324c3d605eae50c5b1945c686038a23
Artem Chebotko <artemchebotko@gmail.com>
2022-07-20 16:21:19 -0500

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo -n 'Waiting for Cassandra to start...'
-timeout 60 bash -c 'until cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
+timeout 60 bash -c 'until cqlsh localhost 9042 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
+timeout 60 bash -c 'until cqlsh localhost 9043 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
 echo ' Ready!'
