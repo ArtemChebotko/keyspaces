e078975dd866246e451aec4e478e1349f308d400
Artem Chebotko <artemchebotko@gmail.com>
2022-07-21 09:46:06 -0500

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -4,3 +4,5 @@ echo -n 'Waiting for Cassandra to start...'
 timeout 60 bash -c 'until cqlsh localhost 9042 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
 timeout 60 bash -c 'until cqlsh localhost 9043 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
 echo ' Ready!'
+
+docker exec -i -t Cassandra-1 bash -c 'nodetool status'
